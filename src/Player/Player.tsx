eafb37a53782a3c0937198ced1d3a9d1e253b249
luksha <lukabjelica@Lukas-MacBook-Pro.local>
2021-04-07 09:57:16 +0200

    Refactor on click handler to functions.

--- src/Player/Player.tsx
+++ src/Player/Player.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState } from 'react';
 import PlayerContext from '../PlayerContext';
 import { BASE_ATTACK, PLAYER_LEFT, PLAYER_RIGHT, POINTS_PER_WIN } from '../constants';
 import './Player.css';
@@ -16,107 +16,122 @@ function PlayerStats({ player, power, turn, points }: PlayerStatsProps) {
 
     // Check oposite player and store to variable
     const opositePlayer = player === PLAYER_LEFT ? PLAYER_RIGHT : PLAYER_LEFT;
+
+    function startCombat() {
+        //If player who is playing round make bigger amount of attack power then oposite
+        if (playersContext.gameplay['player_' + player + '_attack_power'] > playersContext.gameplay['player_' + opositePlayer + '_attack_power']) {
+            updatePlayerStats(player);
+
+            // If oposite player did more attack power
+        } else if (playersContext.gameplay['player_' + player + '_attack_power'] < playersContext.gameplay['player_' + opositePlayer + '_attack_power']) {
+            updatePlayerStats(opositePlayer);
+            // Amount of attack power is same
+        } else {
+            alert("draw!");
+        }
+
+        // Round is finished and if one of player have 0 power check who is winner
+        if (playersContext[player].power === 0 || playersContext[opositePlayer].power === 0) {
+            // Append power to player points
+            playersContext.left.points += playersContext.left.power;
+            playersContext.right.points += playersContext.right.power;
+            // Check is left player winner
+            playersContext.gameplay.winner = getGameWinner();
+        }
+
+        // Reset player values after round is finish
+        resetPlayersStatsAfterCombat();
+    }
+
+    function updatePlayerStats(plyr: string) {
+        // Update points by round winner
+        playersContext[plyr].points += POINTS_PER_WIN;
+        // Update win count
+        playersContext[plyr].winsCount++;
+    }
+
+    function getGameWinner() {
+        // Check is left player winner
+        if (playersContext.left.points > playersContext.right.points) {
+            return 'left - POINTS: ' + playersContext.left.points + ' (power added - ' + playersContext.left.power + ')' + ' (rounds won - ' + playersContext.left.winsCount + ') ||| RIGHT statistics: POINTS: ' + playersContext.right.points + ' (power added - ' + playersContext.right.power + ')' + ' (rounds won - ' + playersContext.right.winsCount + ')';
+            // Check is right player winner
+        } else if (playersContext.left.points < playersContext.right.points) {
+            return 'right - POINTS: ' + playersContext.right.points + ' (power added - ' + playersContext.right.power + ')' + ' (rounds won - ' + playersContext.right.winsCount + ') ||| LEFT statistics: POINTS: ' + playersContext.left.points + ' (power added - ' + playersContext.left.power + ')' + ' (rounds won - ' + playersContext.left.winsCount + ')';
+            //It is draw
+        }
+        return 'No winner! - DRAW';
+
+    }
+
+    function changePlayerTurn() {
+        playersContext[player].turn = !playersContext[player].turn
+        playersContext[opositePlayer].turn = !playersContext[opositePlayer].turn
+    }
+
+    function resetPlayersStatsAfterCombat() {
+        playersContext.left.played = false;
+        playersContext.right.played = false;
+        playersContext.gameplay.player_left_attack_power = BASE_ATTACK;
+        playersContext.gameplay.player_right_attack_power = BASE_ATTACK;
+    }
+
+
     return (
         <>
-         <div className={`playerContainer `}>
-             <div>
-                 {turn ? <div className="player-turn"><b>PLAYING:</b> </div> : null}
-                    <hr/>
-                 <div>Score: <b>{points}</b></div>
-                <div>Attack wins: {playersContext[player].winsCount}</div>
-                 <hr/>
+            <div className={`playerContainer `}>
+                <div>
+                    {turn ? <div className="player-turn"><b>PLAYING:</b> </div> : null}
+                    <hr />
+                    <div>Score: <b>{points}</b></div>
+                    <div>Attack wins: {playersContext[player].winsCount}</div>
+                    <hr />
                     <div><b>Current Power: {power}</b></div>
                     <div>
-                     Attack power {player}
+                        Attack power {player}
                         <input type="number" value={playerAttack} onChange={(e) => { setPlayerAttack(parseInt(e.target.value)) }} /></div>
                     <button onClick={() => {
 
-                     // Check does user have enough power and is it 0 or bigger - 0 is allowed
-                     if (playerAttack > power || playerAttack < 0) {
-                        alert("Invalid player attack amount!");
-                        return;
-                     }
-                     
-                     // If not player turn
-                     if (!turn) {
-                         alert("Not your turn!");
-                         return;
-                     }
-                                          
-                     // Subtract playerPower by amount of his attack
-                     playersContext[player].power = power - playerAttack;
+                        // Check does user have enough power and is it 0 or bigger - 0 is allowed
+                        if (playerAttack > power || playerAttack < 0) {
+                            alert("Invalid player attack amount!");
+                            return;
+                        }
 
+                        // If not player turn
+                        if (!turn) {
+                            alert("Not your turn!");
+                            return;
+                        }
 
-                     // Set played value to turn
-                     playersContext[player].played = true;
+                        // Subtract playerPower by amount of his attack
+                        playersContext[player].power = power - playerAttack;
+                        // Set played value to turn
+                        playersContext[player].played = true;
+                        // Set play attack power value (prepare for round battle)
+                        playersContext.gameplay['player_' + player + '_attack_power'] = playerAttack;
 
-                     // Set play attack power value (prepare for round battle)
-                     playersContext.gameplay['player_' + player + '_attack_power'] = playerAttack;
-                    
-                     // Check if both players have played
+                        // Check if both players have played
+                        if (playersContext.left.played && playersContext.right.played) {
+                            startCombat();
+                        }
 
-                     if (playersContext.left.played && playersContext.right.played) {
-                         //If player who is playing round make bigger amount of attack power then oposite
-                         if (playersContext.gameplay['player_' + player + '_attack_power'] > playersContext.gameplay['player_' + opositePlayer + '_attack_power']) {
-                            // Update points by round winner
-                             playersContext[player].points += POINTS_PER_WIN;
-                             
-                             // Update win count
-                            playersContext[player].winsCount++;
-
-                        // If oposite player did more attack power
-                        } else if (playersContext.gameplay['player_' + player + '_attack_power'] < playersContext.gameplay['player_' + opositePlayer + '_attack_power']) {
-                            playersContext[opositePlayer].points += POINTS_PER_WIN;
-                            playersContext[opositePlayer].winsCount++;
-                        // Amount of attack power is same
-                        } else {
-                            alert("draw!");
-                         }
-                         
-                        // Round is finished and if one of player have 0 power check who is winner
-                         if (playersContext[player].power === 0 || playersContext[opositePlayer].power === 0) {
-                                // Append power to player points
-                                playersContext.left.points += playersContext.left.power;
-                                playersContext.right.points += playersContext.right.power;
-                                // Check is left player winner
-                                if (playersContext.left.points > playersContext.right.points) {
-                                    playersContext.gameplay.winner = 'left - POINTS: ' + playersContext.left.points + ' (power added - ' + playersContext.left.power + ')' + ' (rounds won - ' + playersContext.left.winsCount + ') ||| RIGHT statistics: POINTS: ' + playersContext.right.points + ' (power added - ' + playersContext.right.power + ')' + ' (rounds won - ' + playersContext.right.winsCount + ')';
-                                // Check is right player winner
-                                } else if (playersContext.left.points < playersContext.right.points) {
-                                    playersContext.gameplay.winner = 'right - POINTS: ' + playersContext.right.points + ' (power added - ' + playersContext.right.power + ')' + ' (rounds won - ' + playersContext.right.winsCount + ') ||| LEFT statistics: POINTS: ' + playersContext.left.points + ' (power added - ' + playersContext.left.power + ')' + ' (rounds won - ' + playersContext.left.winsCount + ')';
-                                //It is draw
-                                } else {
-                                    playersContext.gameplay.winner = 'No winner! - DRAW';
-                                }
-                            
-                        } 
-                         
-                        // Reset player values after round is finish
-                        playersContext.left.played = false;
-                        playersContext.right.played = false;
-                        playersContext.gameplay.player_left_attack_power = BASE_ATTACK;
-                        playersContext.gameplay.player_right_attack_power = BASE_ATTACK;
-                    }
-                     
-                     
-                     // Change player turn
-                     playersContext[player].turn = !playersContext[player].turn
-                     playersContext[opositePlayer].turn = !playersContext[opositePlayer].turn
-                     // reset player attack
-                     setPlayerAttack(BASE_ATTACK)
-                     // Update context
-                     setPlayers({ ...playersContext });
-          }}
-          >Attack</button>
-        </div>
-        <div>
-          Attack: {playerAttack}
-             </div>
+                        // Change player turn
+                        changePlayerTurn();
+                        // reset player attack
+                        setPlayerAttack(BASE_ATTACK)
+                        // Update context
+                        setPlayers({ ...playersContext });
+                    }}
+                    >Attack</button>
+                </div>
+                <div>
+                    Attack: {playerAttack}
+                </div>
             </div>
-            </>
+        </>
     )
 
 }
-  
 
-export {PlayerStats}
+
+export { PlayerStats }
